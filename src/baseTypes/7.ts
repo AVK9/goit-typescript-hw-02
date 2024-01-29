878024c8ff8fedfa18d30284cdea819e2a6f8feb
AlexKat <katsubo79@gmail.com>
2024-01-29 16:31:34 +0200

    ++

--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,22 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum WeekDays {
+  Sunday,
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+}
+
+
+function isWeekend(day: WeekDays): boolean {
+   if (day === WeekDays.Saturday || day === WeekDays.Sunday) {
+    return true;
+  } else {
+    return false;
+  }
+}
--- src/generic/2.ts
+++ src/generic/2.ts
@@ -2,6 +2,23 @@
   У вас є тип AllType. Існує функція compare, яка приймає два об'єкти. Ці об'єкти містять поля AllType. 
   Ваше завдання – використовувати Pick та generics для вказівки, що поля цих об'єктів належать AllType.
   Функція compare повинна повертати AllType.
+  
+type AllType = {
+  name: string;
+  position: number;
+  color: string;
+  weight: number
+}
+
+function compare (top, bottom): AllType {
+  return {
+    name: top.name,
+    color: top.color,
+    position: bottom.position,
+    weight: bottom.weight,
+  }
+}
+
 */
 
 type AllType = {
@@ -11,7 +28,7 @@ type AllType = {
   weight: number
 }
 
-function compare (top, bottom): AllType {
+function compare (top: Pick<AllType, 'name' |'color'>, bottom: Pick<AllType, 'position' |'weight'>): AllType {
   return {
     name: top.name,
     color: top.color,
